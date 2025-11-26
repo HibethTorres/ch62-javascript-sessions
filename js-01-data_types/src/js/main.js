ac8b85a75af6c1bda54b122523ac11ad0f3b0e03
Hibeth Torres <hibeth.torres@gmail.com>
2025-11-26 10:56:20 -0600

    feat: Declaracion de variables y muestra en consola

--- js-01-data_types/src/js/main.js
+++ js-01-data_types/src/js/main.js
@@ -1,2 +1,34 @@
 console.log("Session JS01 - Data Types");
 
+/* ================================================================
+   1. VARIABLES Y NOMENCLATURA (5 min)
+   ================================================================
+   Reglas modernas:
+   - 'const': Úsalo por defecto (valores constantes).
+   - 'let': Úsalo solo si el valor va a cambiar (reasignación).
+   - 'var': EVITAR (es obsoleto en desarrollo moderno).
+   
+   Nomenclatura (CamelCase):
+   - Correcto: userAge, firstName, isActive
+   - Incorrecto: user_age, FirstName, is_active
+*/
+
+//TODO: Declara 3 variables para un perfil de usuario ficticio
+
+const userName = "PenelopeGarcia"; // Nombre de usuario - String - const en lugar de let porque no va a cambiar
+let userAge = 33;         // Edad del usuario - Integer
+let userEmotion = "Happy";    // Estado activo del usuario - String
+
+// --- Template Literals (Backticks ``) ---
+// Permiten interpolación ${} y saltos de línea. Más limpio que concatenar con '+'.
+// ejemplo:  `Hola ch62, soy ${userName}`
+
+const firstName = "Daniel";
+let currentAge = 24;
+const currentNationality = "Mexican";
+
+// De las variables anteriores muestra en console el sig. mensaje:
+// "My name is Daniel, I am 24 years old and I am Mexican."
+
+console.log(`My name is ${firstName}, I am ${currentAge} years old and I am ${currentNationality}.`);
+
