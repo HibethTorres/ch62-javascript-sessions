839e1d0f0964765a2ba30ae5952c1ddc23d38ce6
Hibeth Torres <hibeth.torres@gmail.com>
2025-11-28 17:35:05 -0600

    feat: operadores && ||

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -145,3 +145,92 @@ console.log(checkTemperature(20)); //Imprime "It's warm"
 console.log(checkTemperature(10)); //Imprime "It's cold"
 console.log(checkTemperature(-5)); //Imprime "It's freezing"
 
+
+//Operador AND (&&) y OR (||)
+/*
+Operador de cortocircuito.
+
+ OP1 && OP2 Si OP1 es verdadero, se retorna la expresión de OP2.
+ OP1 || OP2 Si OP1 es verdadero, se retorna la expresión de OP1.
+ 
+ Se consideran falsos: "", 0, null, undefined, NaN
+*/
+console.log( true && false ); // false
+console.log( true && "Usando corto circuito "); // "Usando corto circuito "
+console.log( "false" && "Mi mente sigue de vacaciones"); // "Mi mente sigue de vacaciones"
+console.log( 0 && "Chau, chau"); // 0
+console.log( "" && "Chau, chau"); // ""
+console.log( false && "Chau, chau"); // false
+
+console.log( true || false ); // true
+console.log( true || "Usando corto circuito "); // true
+console.log( "false" || "Mi mente sigue de vacaciones"); // "false"
+console.log( 0 || "Chau, chau"); // "Chau, chau"
+console.log( "" || "Chau, chau"); // "Chau, chau"
+console.log( false || "Chau, chau"); // "Chau, chau"
+console.log( false || undefined ); // undefined
+
+console.log( "Activado" && undefined ); // undefined
+
+const isLoggedIn = true;
+if(isLoggedIn) {
+    console.log("El usuario está logueado");
+}  else {
+    console.log("El usuario no está logueado");    
+}
+
+console.log( isLoggedIn && "El usuario está logueado" || "El usuario no está logueado" ); 
+
+/*
+     Realizar una función que reciba un código(weatherCode) de temperatura.
+     De acuerdo al código recibo, retornar un mensaje:
+     Code   Description
+    0           Clear sky
+    1, 2, 3     Mainly clear, partly cloudy, and overcast
+    45, 48      Fog and depositing rime fog
+    
+*/
+
+function getWeather(weatherCode) { //Funcion que recibe un código weatherCode
+   
+    let description = "";
+
+    if (weatherCode === 0) {
+        description = "Clear sky";
+    } else if (weatherCode === 1 || weatherCode === 2 || weatherCode === 3) {
+        description = "Mainly clear, partly cloudy, and overcast";
+    } else if (weatherCode === 45 || weatherCode === 48) {
+        description = "Fog and depositing rime fog";
+    } else {
+        description = "Unknown weather code";
+    }
+    return description;
+};
+
+//--------------- Condicional Switch-case --------------------------
+/*
+ La condicional Switch evalua una expresión y se compara
+ con el valor de cada instancia en 'case' y se ejecuta las
+ sentencias asociadas a ese 'case' hasta encontrar
+ la sentencia 'break' o se finalicen todas las sentencias
+ de la condicional switch.
+
+ Para comparar la condicional switch usa el operador estricto ===
+
+ Sintaxis:
+  switch (expresión) {
+    case valor1:
+        sentencia;
+        break;
+    case valor 2:
+        sentencias;
+        break;
+    case valor n:
+        sentencias;
+        break
+    default:
+        sentencias;
+        break;
+  }
+
+*/
