400bd98ff009c4ed625acfac76da4c8f97c75307
Hibeth Torres <hibeth.torres@gmail.com>
2025-12-03 11:31:11 -0600

    feat: add changeLogo module

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -2,5 +2,12 @@
 //Importacion nombrada
 
 import { changePrincipalTitle } from "./modules/changePrincipalTitle.js"; //No olvidar agregar la extension .js
+import { changeLogo } from "./modules/changeLogo.js";
 
 changePrincipalTitle("title");
+
+//Crear una funcion que se le pase el nombre de la imagen a mostar (cat o dino)
+// Esta imagen es la que se debe mostrar en el elemento con id "logo"
+changeLogo("cat.jpg");
+
+
