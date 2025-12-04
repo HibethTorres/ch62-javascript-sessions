8f96b2e23ddd81bd1cef5c14d58c6e74871c529d
Hibeth Torres <hibeth.torres@gmail.com>
2025-12-03 23:37:04 -0600

    feat: the end

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -4,6 +4,7 @@
 import { changePrincipalTitle } from "./modules/changePrincipalTitle.js"; //No olvidar agregar la extension .js
 import { changeLogo } from "./modules/changeLogo.js";
 import { aliceUser } from "./modules/literalObject.js";
+import { User } from "../models/User.js";
 
 changePrincipalTitle("title");
 
@@ -14,4 +15,12 @@ changeLogo("cat.jpg");
 console.log( aliceUser.getFullName() );
 console.log( aliceUser );
 console.log( aliceUser.stringify() );
+
+const fatima = new User("Fatima", "Orozco", 25, "fatima@gmail.com");
+const juan = new User("Juan", "Perez", 28, "juan@gmail.com");
+const luis = new User("Luis", "Martinez", 32, "luis@gmail.com");
+
+console.log( luis.greet() ); //Hello, I'm Luis Martinez, 32 years old. \
+console.log( juan.greet() );
+console.log( fatima.greet() );
 
