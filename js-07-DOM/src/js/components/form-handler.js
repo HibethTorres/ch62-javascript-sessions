6ac7fa655d4cd985fe137146754061b8b2d5814e
Hibeth Torres <hibeth.torres@gmail.com>
2025-12-19 22:38:05 -0600

    feat: ultima actualizacion RESET de formularios

--- js-07-DOM/src/js/components/form-handler.js
+++ js-07-DOM/src/js/components/form-handler.js
@@ -2,14 +2,26 @@
  * Modulo para manejar formularios
  * @returns {object} objeto con los inputs del formulario
  */
-const formHandler = () => {
+const formHandler = (formReference) => {
+
+    /*
     const tasktitle = document.getElementById("taskTitle").value; //Para leer formulario usamos .value, usado para la mayoria de los inputs
     //Para checkbox usamos .checked
+    const assignedTo = document.getElementById("assignedTo").value;
 
 
     return {
-        tasktitle: tasktitle  // nombre:atributo
-    };
+        tasktitle: tasktitle,  // nombre:atributo
+        assignedTo: assignedTo
+    }; */
+
+        const formData = new FormData(formReference); //Crea un objeto FormData a partir del formulario
+        const data = Object.fromEntries(formData.entries()); //Convierte FormData a un objeto normal
+        return data; //Devuelve el objeto con los datos del formulario
+/*
+FormData: Una fomrra mas sencilla de manejar formularios en JS
+Permite recopilar y gestionar los datos de un formulario de manera sencilla.
+*/
 };
 
 export { formHandler }; //exportacion nombrada
--- js-07-DOM/src/js/components/setupEventListener.js
+++ js-07-DOM/src/js/components/setupEventListener.js
@@ -11,8 +11,13 @@ const setupEventListener = () => {
     taskForm.addEventListener("submit", (event) => { /* Arrow function dentro del Event Listener */
         event.preventDefault(); //Evita que la pagina se recargue al enviar el formulario
         console.log("Form submitted!");
-        const data = formHandler(); //Llamamos a la funcion del modulo form-handler
+        const data = formHandler(taskForm); //Llamamos a la funcion del modulo form-handler
         console.table(data); //Mostramos los datos en formato tabla
+
+        //Agregar en uan nueva funcion
+        localStorage.setItem("taskData", JSON.stringify(data)); //Guardamos los datos en localStorage como string JSON y JSON.stringify convierte un objeto JS a string JSON para guardar objectos en local storage
+
+        taskForm.reset(); //Reseteamos el formulario para limpiar los campos
     });
 
 }
